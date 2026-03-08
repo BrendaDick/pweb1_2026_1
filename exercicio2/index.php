be47bd405643c08b4f2341b6e7ce4bbeaf7b32a2
BrendaDick <brenda.d2007@aluno.ifsc.edu.br>
2026-03-08 20:54:55 -0300

    commit

---
+++ exercicio2/index.php
@@ -0,0 +1,19 @@
+<?php
+#Exercício01
+echo "<h2>Página de Matrícula</h2>";
+echo "<p>Aqui será desenvolvido o formulário de matrícula.</p>";
+echo "<a href='index.php'>Voltar ao Menu</a>";
+?>
+
+<?php
+#Exercício02
+echo "<h2>Ficha Cadastral</h2>";
+echo "<p>Conteúdo da Ficha Cadastral em desenvolvimento...</p>";
+echo "<a href='index.php'>Voltar ao Menu</a>";
+?>
+
+<?php
+#Exercício03
+echo "<h2>Lista de Inscritos</h2>";
+echo "<a href='index.php'>Voltar ao Menu</a>";
+?>
